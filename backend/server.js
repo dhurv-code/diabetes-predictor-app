0b0082ac7558c21bc85443e1d326fefd09970bd3
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-04-08 11:09:44 +0530

    updated cors port

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cors=require("cors")
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://diabetes-predictor-app-vert.vercel.app/",
   credentials: true
 }));
 
