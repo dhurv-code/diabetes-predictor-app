97738c7a9c44895b285ecbca22435a10a6e5458c
Dhurv Gupta <dhurvgupta143@gmail.com>
2026-04-08 17:07:07 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const authroutes=require("./routes/authRoute")
 const healthRoutes=require("./routes/healthRoute")
 const cookieParser=require("cookie-parser")
 const cors=require("cors")
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -12,7 +13,6 @@ app.use(cors({
   origin: "https://diabetes-predictor-app-dusky.vercel.app",
   credentials: true
 }));
-app.use(cors());
 
 
 app.use(express.json());
@@ -29,6 +29,6 @@ app.get("/", (req, res) => {
 app.use("/auth",authroutes)
 app.use("/health",healthRoutes)
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
